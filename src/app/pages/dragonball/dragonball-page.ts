0f6173c1ffec00f54069055022a5a8266065de23
llondo <luis.londob81@gmail.com>
2025-09-13 16:12:15 -0500

    S5/L09 actualizacion 20250913_1612

--- src/app/pages/dragonball/dragonball-page.ts
+++ src/app/pages/dragonball/dragonball-page.ts
@@ -1,5 +1,5 @@
 import { Component, computed, signal } from '@angular/core';
-import { NgClass } from '@angular/common';
+// import { NgClass } from '@angular/common';
 
 interface Character {
   id: number;
@@ -16,11 +16,32 @@ interface Character {
   ],
 })
 export class DragonballPage {
+  name = signal('Gohan');
+  power = signal(100);
+
   characters = signal<Character[]>([
     { id: 1, name: 'Goku', power: 15000 },
     { id: 2, name: 'Vegeta', power: 12000 },
     { id: 3, name: 'Piccolo', power: 3500 },
+    { id: 4, name: 'Yamcha', power: 500 },
   ]);
+
+  addCharacter() {
+    if (this.name() === '' || this.power() <= 0) return;
+
+    const newCharacter: Character = {
+      id: Math.max(...this.characters().map((c) => c.id)) + 1,
+      name: this.name(),
+      power: this.power(),
+    };
+    this.characters.update((chars) => [...chars, newCharacter]);
+    this.resetFields();
+  }
+
+  resetFields() {
+    this.name.set('');
+    this.power.set(0);
+  }
 
   // powerClasses = computed(() => {
   //   return {
